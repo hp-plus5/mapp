7edc8fdac8f7326215f6d69f3ba197e3839c1a9b
Sam Danger Warner <samwarner@duck.com>
2024-07-18 12:54:02 -0400

    Simplify SCSS imports to reduce redundant instances of each style in app.scss

--- src/Mapp/Mapp.js
+++ src/Mapp/Mapp.js
@@ -1,11 +1,10 @@
-import './Mapp.scss';
 import UploadCsv from "./UploadCsv";
 import Map from "./Map";
 import SavedCollectionList from './SavedCollectionList';
 
 export default function Mapp() {
     return(
-        <div id="mapp">
+        <div>
             <UploadCsv />
             <Map />
             <SavedCollectionList />
